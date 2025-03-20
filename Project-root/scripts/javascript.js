f1c53b52c4abb2fde28b4a619fe6e1d1d289a375
Jatin0508 <gollajatin2005@gmail.com>
2025-03-20 19:42:28 +0530

    Add files via upload

---
+++ Project-root/scripts/javascript.js
@@ -0,0 +1,4 @@
+document.querySelector('.get-started').addEventListener('click', () => {
+    // Replace 'generate-comic.html' with the name of your next page
+    window.location.href = 'generate-comic.html';
+});
