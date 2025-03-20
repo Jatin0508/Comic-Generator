25503ba6206506ff96782d52ae6d6d73a15cc584
Jatin0508 <gollajatin2005@gmail.com>
2025-03-20 19:34:35 +0530

    Delete Project-root directory

--- Project-root/scripts/javascript.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.get-started').addEventListener('click', () => {
-    // Replace 'generate-comic.html' with the name of your next page
-    window.location.href = 'generate-comic.html';
-});
